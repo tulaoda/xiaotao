cecc6f95d4151f6244811e7113d646a4e15a60d2
songxinxin <2513691817@qq.com>
2018-05-14 19:56:35 +0800

    格式化代码

--- NetXiaoTao/src/com/xt/entity/Goods.java
+++ NetXiaoTao/src/com/xt/entity/Goods.java
@@ -16,136 +16,138 @@ import javax.persistence.Table;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 
-@Entity  
+@Entity
 @Table(name = "t_goods")
 public class Goods {
-  @Id
-  @GeneratedValue(strategy=GenerationType.AUTO)
-  @Column(name = "id")
-  private Long id;
-  
-  @Column(name = "l_class", length = 11)
-  private Long l_class;
-  
-  @Column(name = "content", length = 100)
-  private String content;
-  
-  @Column(name = "price", length = 0)
-  private float price;
-  
-  @Column(name = "userid", length = 45)
-  private String userid;
-  
-  @Column(name = "state", length = 11)
-  private Long state;
-  
-  @Column(name = "proxy", length = 1)
-  private Long proxy;
-  
-  @Column(name = "stock", length = 11)
-  private Long stock;
-  
-  @Column(name = "postage", length = 1)
-  private Long postage;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private Long id;
 
-  @OneToMany(targetEntity=GoodsImg.class,cascade=CascadeType.ALL)  
-  @Fetch(FetchMode.JOIN)  
-  //updatable=false很关键，如果没有它，在级联删除的时候就会报错(反转的问题)  
-  @JoinColumn(name="goodsid",updatable=false)  
-  private Set<GoodsImg> goodsImg = new HashSet<GoodsImg>();  
-  public Set<GoodsImg> getGoodsImg() {  
-  return goodsImg;  
-  }  
-  public void setSets(Set<GoodsImg> goodsImg) {  
-  this.goodsImg = goodsImg;  
-  }  
-  public Goods() {
-	super();
-}
+	@Column(name = "l_class", length = 11)
+	private Long l_class;
 
-public Goods(Long id, Long l_class, String content, float price,
-		String userid, Long state, Long proxy, Long stock, Long postage) {
-	super();
-	this.id = id;
-	this.l_class = l_class;
-	this.content = content;
-	this.price = price;
-	this.userid = userid;
-	this.state = state;
-	this.proxy = proxy;
-	this.stock = stock;
-	this.postage = postage;
-}
+	@Column(name = "content", length = 100)
+	private String content;
 
-public Long getId() {
-    return id;
-  }
+	@Column(name = "price", length = 0)
+	private float price;
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+	@Column(name = "userid", length = 45)
+	private String userid;
 
-  public Long getL_class() {
-    return l_class;
-  }
+	@Column(name = "state", length = 11)
+	private Long state;
 
-  public void setL_class(Long l_class) {
-    this.l_class = l_class;
-  }
+	@Column(name = "proxy", length = 1)
+	private Long proxy;
 
-  public String getContent() {
-    return content;
-  }
+	@Column(name = "stock", length = 11)
+	private Long stock;
 
-  public void setContent(String content) {
-    this.content = content;
-  }
+	@Column(name = "postage", length = 1)
+	private Long postage;
 
-  public float getPrice() {
-    return price;
-  }
+	@OneToMany(targetEntity = GoodsImg.class, cascade = CascadeType.ALL)
+	@Fetch(FetchMode.JOIN)
+	@JoinColumn(name = "goodsid", updatable = false)
+	private Set<GoodsImg> goodsImg = new HashSet<GoodsImg>();
 
-  public void setPrice(float price) {
-    this.price = price;
-  }
+	public Set<GoodsImg> getGoodsImg() {
+		return goodsImg;
+	}
 
-  public String getUserid() {
-    return userid;
-  }
+	public void setSets(Set<GoodsImg> goodsImg) {
+		this.goodsImg = goodsImg;
+	}
 
-  public void setUserid(String userid) {
-    this.userid = userid;
-  }
+	public Goods() {
+		super();
+	}
 
-  public Long getState() {
-    return state;
-  }
+	public Goods(Long id, Long l_class, String content, float price,
+			String userid, Long state, Long proxy, Long stock, Long postage) {
+		super();
+		this.id = id;
+		this.l_class = l_class;
+		this.content = content;
+		this.price = price;
+		this.userid = userid;
+		this.state = state;
+		this.proxy = proxy;
+		this.stock = stock;
+		this.postage = postage;
+	}
 
-  public void setState(Long state) {
-    this.state = state;
-  }
+	public Long getId() {
+		return id;
+	}
 
-  public Long getProxy() {
-    return proxy;
-  }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-  public void setProxy(Long proxy) {
-    this.proxy = proxy;
-  }
+	public Long getL_class() {
+		return l_class;
+	}
 
-  public Long getStock() {
-    return stock;
-  }
+	public void setL_class(Long l_class) {
+		this.l_class = l_class;
+	}
 
-  public void setStock(Long stock) {
-    this.stock = stock;
-  }
+	public String getContent() {
+		return content;
+	}
 
-  public Long getPostage() {
-    return postage;
-  }
+	public void setContent(String content) {
+		this.content = content;
+	}
 
-  public void setPostage(Long postage) {
-    this.postage = postage;
-  }
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public Long getState() {
+		return state;
+	}
+
+	public void setState(Long state) {
+		this.state = state;
+	}
+
+	public Long getProxy() {
+		return proxy;
+	}
+
+	public void setProxy(Long proxy) {
+		this.proxy = proxy;
+	}
+
+	public Long getStock() {
+		return stock;
+	}
+
+	public void setStock(Long stock) {
+		this.stock = stock;
+	}
+
+	public Long getPostage() {
+		return postage;
+	}
+
+	public void setPostage(Long postage) {
+		this.postage = postage;
+	}
 }
